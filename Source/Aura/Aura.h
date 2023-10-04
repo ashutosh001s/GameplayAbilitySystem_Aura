5970b0a2f2529bb2c9cf5240a19889b1a74a5cd5
Ashutosh Singh <66360078+ashutosh001s@users.noreply.github.com>
2023-10-04 09:04:32 +0530

    Added base character class

---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Ashutosh singh
+
+#pragma once
+
+#include "CoreMinimal.h"
+
---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,13 @@
+// Copyright Ashutosh singh
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
